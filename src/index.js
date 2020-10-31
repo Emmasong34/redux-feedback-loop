99654442b36e7fc1a45d290d4fa9d3492432eaeb
Emma Song Fisher <emmasongf@gmail.com>
2020-10-30 21:54:45 -0500

    components created

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+
+//holds all reducers
+const storeInstance = createStore(
+    combineReducers({
+        
+    }),    
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
