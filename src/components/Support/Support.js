3494217eca840b99d282b3d1252556d180df27a6
Emma Song Fisher <emmasongf@gmail.com>
2020-11-01 00:51:24 -0500

    on change added to support component

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -3,21 +3,39 @@ import {connect} from 'react-redux';
 
 class Support extends Component {
 
+    state = {
+        support: 0
+    }
+
     changePage = () => {
         console.log('changing pages');
         this.props.history.push('/comments');
+    }
+
+    addSupport = (event) => {
+        console.log('in add support', this.state.support);
+        event.preventDefault();
+        this.props.dispatch({type: 'COLLECT_SUPPORT', payload: this.state.support})
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            support: {
+                support: [event.target.value]
+            }
+        })
     }
 
 
     render () {
         return (
 
-            <div>
-                  <h2>How well are you being supported</h2>
-                  <p>support?</p>
-                <input type="number" placeholder="1-5"></input>
+            <form onClick={this.addSupport}>
+                <h2>How well are you being supported</h2>
+                <p>support?</p>
+                <input className="supportInput" type="number" placeholder="1-5" onChange={this.handleChange}></input>
                 <button onClick={this.changePage}>Next</button>
-            </div>
+            </form>
         )
     }
 }
